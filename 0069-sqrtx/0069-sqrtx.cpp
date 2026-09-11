80b432cb580e0e603f937efe3ab213f4d64215b0
mayank17032006-creator <mayank17032006@gmail.com>
2026-09-11 15:32:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.2%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+        int low = 1;
+
+        int high = x;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(1LL*mid*mid == x){
+                return mid;
+            }
+            else if(1LL*mid*mid > x){
+                high = mid-1;
+            }
+            else if(1LL*mid*mid < x) {
+                low = mid+1; 
+            }
+
+        }
+        return int(high);
+    }
+};
