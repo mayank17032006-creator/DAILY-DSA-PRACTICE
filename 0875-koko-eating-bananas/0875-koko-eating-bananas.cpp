c9780e080b8f2ed515b5a4f7ae87d9ddd94bdff3
mayank17032006-creator <mayank17032006@gmail.com>
2026-09-19 00:54:16 +0530

    Time: 9 ms (43.99%), Space: 22.8 MB (89.92%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    long long calculate(vector<int>& piles,long long mid){
-        long long ans = 0;
-        for(int i=0;i<piles.size();i++){
-            ans = ans+(piles[i]+mid-1)/mid;
-
-        }
-        return ans;
-    }
+   
     int minEatingSpeed(vector<int>& piles, int h) {
-        int answer = INT_MAX;
+        
         int low = 1;
         int high = *max_element(piles.begin(),piles.end());
+        int ans = high;
         while(low<=high){
+
             int mid = low+(high-low)/2;
-            long long val = calculate(piles,mid);
-            if(val<=h){
-                answer = min(answer,mid);
+            long long val=0 ;
+            for(int i=0;i<piles.size();i++){
+                val=val+(piles[i]+mid-1)/mid;
+            }
+
+             if(val<=h){
+                ans = min(ans,mid);
                 high = mid-1;
             }
             else{
@@ -24,7 +23,7 @@ public:
                 }
 
         }
-        return answer;
         
+        return ans;
     }
 };
